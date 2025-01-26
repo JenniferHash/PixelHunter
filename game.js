9f0a9e71e107677251f9481553e970eb474af68f
JenniferHash <n9uu4y9wks2@hotmail.com>
2025-01-26 21:11:10 +0800

    player control box

--- game.js
+++ game.js
@@ -21,6 +21,15 @@ class GameScene extends Phaser.Scene {
                 </svg>
             `));
         });
+        
+        this.load.image('player', 'data:image/svg+xml;base64,' + btoa(`
+            <svg width="60" height="60" xmlns="http://www.w3.org/2000/svg">
+                <rect width="60" height="60" fill="#ffffff" stroke="#000" stroke-width="3"/>
+                <circle cx="20" cy="20" r="5" fill="#000"/>
+                <circle cx="40" cy="20" r="5" fill="#000"/>
+                <path d="M 15 40 Q 30 50 45 40" stroke="#000" stroke-width="2" fill="none"/>
+            </svg>
+        `));
     }
 
     create() {
@@ -33,13 +42,15 @@ class GameScene extends Phaser.Scene {
         });
         welcomeText.setOrigin(0.5, 0.5);
 
-        const instructionText = this.add.text(400, 500, 'Click the moving squares!', {
+        const instructionText = this.add.text(400, 500, 'Use WASD to move, click the squares!', {
             fontSize: '18px',
             fill: '#ffffff',
             fontFamily: 'Arial'
         });
         instructionText.setOrigin(0.5, 0.5);
 
+        this.player = this.add.image(400, 300, 'player');
+        
         this.currentColorIndex1 = 0;
         this.currentColorIndex2 = 1;
         
@@ -57,6 +68,9 @@ class GameScene extends Phaser.Scene {
             console.log('Square 2 clicked!');
         });
 
+        this.cursors = this.input.keyboard.createCursorKeys();
+        this.wasd = this.input.keyboard.addKeys('W,S,A,D');
+
         this.moveSquare(this.square1, 1);
         this.moveSquare(this.square2, 2);
 
@@ -64,7 +78,23 @@ class GameScene extends Phaser.Scene {
     }
 
     update() {
+        const playerSpeed = 200;
         
+        if (this.cursors.left.isDown || this.wasd.A.isDown) {
+            this.player.x -= playerSpeed * this.game.loop.delta / 1000;
+        }
+        if (this.cursors.right.isDown || this.wasd.D.isDown) {
+            this.player.x += playerSpeed * this.game.loop.delta / 1000;
+        }
+        if (this.cursors.up.isDown || this.wasd.W.isDown) {
+            this.player.y -= playerSpeed * this.game.loop.delta / 1000;
+        }
+        if (this.cursors.down.isDown || this.wasd.S.isDown) {
+            this.player.y += playerSpeed * this.game.loop.delta / 1000;
+        }
+        
+        this.player.x = Phaser.Math.Clamp(this.player.x, 30, 770);
+        this.player.y = Phaser.Math.Clamp(this.player.y, 30, 570);
     }
 
     moveSquare(square, squareId) {
